0da4a3999a77dcef2609b8db1441ef376af709fd
unknown <naik.khushi5423@gmail.com>
2020-03-17 21:30:02 +0530

    fourth commit

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ var app=express()
 //var testImg = require('path').join(__dirname,'/public'); 
 //app.use(express.static(testImg));
 var nStatic = require('node-static');
-
+const mongoose = require('mongoose'); 
+mongoose.connect('mongodb://localhost:27017/user'); 
+var db=mongoose.connection; 
+db.on('error', console.log.bind(console, "connection error")); 
+db.once('open', function(callback){ 
+    console.log("connection succeeded"); 
+}) 
+  
 var fileServer = new nStatic.Server('./public');
 
 http.createServer(function (req, res) {
@@ -32,11 +39,58 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/views/index.html'));
 }); 
 
+app.get('/signup',function(req,res){
+    res.sendFile(path.join(__dirname+'/views/signup.html'));
+}); 
+
+app.post('/sign_up', function(req,res){ 
+    var fname = req.body.first_name; 
+    var lname = req.body.last_name;
+    var email =req.body.email; 
+    var pass = req.body.password; 
+     
+    
+    var data = { 
+        "f_name": fname,
+        "l_name": lname, 
+        "email":email, 
+        "password":pass,  
+    } 
+
+    console.log(data.name);
+    db.collection('user_details').insertOne(data,function(err, collection){ 
+         if (err) throw err; 
+         console.log("Record inserted Successfully"); 
+              
+    }); 
+    //res.send("values taken");    
+    res.sendFile(path.join(__dirname+'/views/submission.html'));
+    //return res.redirect('signup_success.html'); 
+});
+
 app.get('/destination',function(req,res){
     
     res.sendFile(path.join(__dirname+'/views/destination.html'));
 });
 
+app.get('/destination/asia',function(req,res){    
+    res.sendFile(path.join(__dirname+'/views/asia.html'));
+});
+app.get('/destination/europe',function(req,res){    
+    res.sendFile(path.join(__dirname+'/views/europe.html'));
+});
+app.get('/destination/africa',function(req,res){    
+    res.sendFile(path.join(__dirname+'/views/africa.html'));
+});
+app.get('/destination/australia',function(req,res){    
+    res.sendFile(path.join(__dirname+'/views/australia.html'));
+});
+app.get('/destination/namerica',function(req,res){    
+    res.sendFile(path.join(__dirname+'/views/namerica.html'));
+});
+app.get('/destination/samerica',function(req,res){    
+    res.sendFile(path.join(__dirname+'/views/samerica.html'));
+});
 app.listen(3000,function(){
     console.log("connected to server");
 })
