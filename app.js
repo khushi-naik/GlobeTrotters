8b6fe708549644dd5ee46b255d50d7e9d6a748e7
Joshua Poddoku <201751033@iiitvadodara.ac.in>
2020-05-12 21:32:48 +0530

    na footer fixed

--- app.js
+++ app.js
@@ -22,12 +22,11 @@ app.set('view engine', 'ejs');
 var nStatic = require('node-static');
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/blog', ({useUnifiedTopology: true, useNewUrlParser: true}));
+mongoose.connect('mongodb://localhost:27017/gfg', ({useUnifiedTopology: true, useNewUrlParser: true}));
 var db = mongoose.connection;
 db.on('error', console.log.bind(console, "connection error"));
-db.once('open', function (callback) {
     console.log("connection succeeded");
-})
+
 
 
 var fileServer = new nStatic.Server('./public');
